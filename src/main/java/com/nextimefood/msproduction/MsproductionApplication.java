c011acdb8ede76c43eeab761e78293c56b36c1d3
MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
2025-12-19 23:58:00 -0300

    fix: resetando proj

---
+++ src/main/java/com/nextimefood/msproduction/MsproductionApplication.java
@@ -0,0 +1,13 @@
+package com.nextimefood.msproduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsproductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsproductionApplication.class, args);
+	}
+
+}
